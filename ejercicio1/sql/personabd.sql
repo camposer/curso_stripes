d80cf8df34c01d268608d00d2bb2dc10992635ba
Rodolfo Campos <camposer@gmail.com>
2015-03-25 18:55:48 +0100

    Fichero persona.sql

---
+++ ejercicio1/sql/personabd.sql
@@ -0,0 +1,9 @@
+CREATE TABLE persona (
+	id INTEGER NOT NULL 
+		GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+	nombre VARCHAR(50) NOT NULL,
+	apellido VARCHAR(50) NOT NULL,
+	fecha_nacimiento DATE,
+	correo_electronico VARCHAR(50),
+	PRIMARY KEY (id)
+);
